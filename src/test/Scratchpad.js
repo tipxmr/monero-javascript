59a14fced5757009615b40609b3f793980156d33
woodser <woodser@protonmail.com>
2022-08-25 15:22:07 -0400

    update scratchpad

--- src/test/Scratchpad.js
+++ src/test/Scratchpad.js
@@ -1,49 +1,62 @@
 const TestUtils = require("./utils/TestUtils");
 const WalletSyncPrinter = require("./utils/WalletSyncPrinter");
 const monerojs = require("../../index");
+const MoneroConnectionManager = monerojs.MoneroConnectionManager;
+const MoneroConnectionManagerListener = monerojs.MoneroConnectionManagerListener;
+const MoneroRpcConnection = monerojs.MoneroRpcConnection;
+const GenUtils = monerojs.GenUtils;
 
 describe("Scratchpad", function() {
   
   it("Can be scripted easily", async function() {
     
-//    let daemon = await TestUtils.getDaemonRpc();
-//    let walletRpc = await TestUtils.getWalletRpc();
-//    let walletFull = await TestUtils.getWalletFull();
-    
-    // initialize daemon rpc client
-    let daemon = await monerojs.connectToDaemonRpc({
-      uri: "http://localhost:28081",
-      username: "superuser",
-      password: "abctesting123",
-      proxyToWorker: TestUtils.PROXY_TO_WORKER,
-      rejectUnauthorized: false
-    });
-    console.log("Daemon height: " + await daemon.getHeight());
-    
-    // initialize wallet rpc client
-    let walletRpc = await monerojs.connectToWalletRpc({
-      uri: "http://localhost:28084",
-      username: "rpc_user",
-      password: "abc123",
-      rejectUnauthorized: false
-    });
-    await walletRpc.openWallet("test_wallet_1", "supersecretpassword123");
-    console.log("RPC wallet mnemonic: " + await walletRpc.getMnemonic());
-    
     // create in-memory wallet with mnemonic
     let walletFull = await monerojs.createWalletFull({
-      //path: "./test_wallets/" + GenUtils.getUUID(), // in-memory wallet if not given
       password: "abctesting123",
-      networkType: "testnet",
-      serverUri: "http://localhost:28081",
-      mnemonic: "silk mocked cucumber lettuce hope adrenalin aching lush roles fuel revamp baptism wrist long tender teardrop midst pastry pigment equip frying inbound pinched ravine frying",
-      restoreHeight: 0,
+      networkType: "mainnet",
       proxyToWorker: TestUtils.PROXY_TO_WORKER,
       rejectUnauthorized: false
     });
-    await walletFull.sync(new WalletSyncPrinter());
-    console.log("Full wallet daemon height: " + await walletFull.getDaemonHeight());
+    
+    console.log("Full wallet daemon height: " + await walletFull.getHeight());
     console.log("Full wallet mnemonic: " + await walletFull.getMnemonic());
+    
+    // initialize connection manager
+    let connectionManager = new MoneroConnectionManager();
+    await connectionManager.addConnection(new MoneroRpcConnection("https://fallacy.fiatfaucet.com:18089"));
+    await connectionManager.addConnection(new MoneroRpcConnection("https://community.organic-meatballs.duckdns.org:443"));
+    await connectionManager.checkConnections();
+    await connectionManager.startCheckingConnection();
+
+    // listen for connection changes
+    let restoreHeight = undefined;
+    connectionManager.addListener(new class extends MoneroConnectionManagerListener {
+      async onConnectionChanged(connection) {
+        console.log("connection changed: ");
+        console.log(connection);
+        try {
+          if (connection && connection.isConnected()) {
+            await walletFull.setDaemonConnection(connection);
+            if (restoreHeight === undefined) {
+              restoreHeight = await walletFull.getDaemonHeight() - 1;
+            }
+            await walletFull.setSyncHeight(restoreHeight);
+            await walletFull.startSyncing(5000);
+          } else {
+            console.log("Not connected, stopping syncing");
+            await walletFull.stopSyncing();
+          }
+        } catch (err) {
+            console.error(err);
+        }
+      }
+    });
+    
+    // change connections
+    await connectionManager.setConnection("https://fallacy.fiatfaucet.com:18089");
+    await GenUtils.waitFor(10000);
+    await connectionManager.setConnection("https://community.organic-meatballs.duckdns.org:443");
+    await GenUtils.waitFor(10000);
     
     await walletFull.close();
   });
